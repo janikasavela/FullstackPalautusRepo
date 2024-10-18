5da6ebb2a18f0c39004c4a98ef5ed6f4ff6f742e
Janika Savela <janika_s@hotmail.com>
2024-10-18 07:59:30 +0300

    tehtäviä

--- Osa 6/6.3-6.13/src/components/AnecdoteForm.jsx
+++ Osa 6/6.3-6.13/src/components/AnecdoteForm.jsx
@@ -4,15 +4,17 @@ import {
   setNotification,
   clearNotification,
 } from '../reducers/notificationReducer'
+import anecdoteService from '../../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (e) => {
+  const addAnecdote = async (e) => {
     e.preventDefault()
     const content = e.target.anecdote.value
     e.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
     dispatch(setNotification(`You added a new anecdote: '${content}'`))
 
     setTimeout(() => {
