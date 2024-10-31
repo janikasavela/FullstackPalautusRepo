1abf0da3b525463a6f5a9367151eb28397c82465
Janika Savela <janika_s@hotmail.com>
2024-10-31 13:53:30 +0200

    lisaa osan 8 tehtäviä

---
+++ Osa 8/8.8-8.12/src/components/Books.jsx
@@ -0,0 +1,50 @@
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+      id
+      genres
+    }
+  }
+`
+
+const Books = (props) => {
+  const books = useQuery(ALL_BOOKS)
+
+  if (!props.show) {
+    return null
+  }
+
+  if (books.loading) {
+    return <p>loading books..</p>
+  }
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {books.data.allBooks.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Books
