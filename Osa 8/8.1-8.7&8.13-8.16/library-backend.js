54fa43937b29fd71d604a8695311c826e747ee5f
Janika Savela <janika_s@hotmail.com>
2024-11-03 17:27:55 +0200

    8 tehtävät 18-25

--- Osa 8/8.1-8.7&8.13-8.16/library-backend.js
+++ Osa 8/8.1-8.7&8.13-8.16/library-backend.js
@@ -1,6 +1,14 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { expressMiddleware } = require('@apollo/server/express4')
 const { GraphQLError } = require('graphql')
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require('@apollo/server/plugin/drainHttpServer')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
@@ -8,6 +16,11 @@ const Author = require('./models/author')
 const Book = require('./models/book')
 const User = require('./models/user')
 const jwt = require('jsonwebtoken')
+const { WebSocketServer } = require('ws')
+const { useServer } = require('graphql-ws/lib/use/ws')
+
+const { PubSub } = require('graphql-subscriptions')
+const pubsub = new PubSub()
 
 require('dotenv').config()
 
@@ -25,6 +38,10 @@ mongoose
   })
 
 const typeDefs = `
+type Subscription {
+  bookAdded: Book!
+}    
+
  type User {
   username: String!
   favoriteGenre: String
@@ -147,6 +164,9 @@ const resolvers = {
 
       try {
         await book.save()
+        await pubsub.publish('BOOK_ADDED', {
+          bookAdded: await book.populate('author'),
+        })
         return book.populate('author')
       } catch (error) {
         throw new GraphQLError('Saving book failed', {
@@ -219,8 +239,13 @@ const resolvers = {
       return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
     },
   },
+  Subscription: {
+    bookAdded: {
+      subscribe: () => pubsub.asyncIterator('BOOK_ADDED'),
+    },
+  },
 }
-
+/* 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -241,4 +266,61 @@ startStandaloneServer(server, {
   },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
-})
+}) */
+
+const start = async () => {
+  const app = express()
+  const httpServer = http.createServer(app)
+
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/',
+  })
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
+  const serverCleanup = useServer({ schema }, wsServer)
+
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose()
+            },
+          }
+        },
+      },
+    ],
+  })
+
+  await server.start()
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.startsWith('Bearer ')) {
+          const decodedToken = jwt.verify(
+            auth.substring(7),
+            process.env.JWT_SECRET
+          )
+          console.log('Decoded Token:', decodedToken)
+          const currentUser = await User.findById(decodedToken.id)
+          console.log('Current User', currentUser)
+          return { currentUser }
+        }
+        return {} // Lisää tämä palauttamaan tyhjä objekti, jos token puuttuu
+      },
+    })
+  )
+  const PORT = 4000
+  httpServer.listen(PORT, () =>
+    console.log(`Server is now running on http://localhost:${PORT}`)
+  )
+}
+start()
--- Osa 8/8.8-8.12/src/components/Books.jsx
+++ Osa 8/8.8-8.12/src/components/Books.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { gql, useQuery } from '@apollo/client'
+import { gql, useQuery, useSubscription } from '@apollo/client'
 
 const ALL_BOOKS = gql`
   query allBooks($genre: String) {
@@ -25,6 +25,22 @@ const ALL_GENRES = gql`
   }
 `
 
+const BOOK_ADDED = gql`
+  subscription {
+    bookAdded {
+      title
+      published
+      author {
+        name
+        born
+        id
+      }
+      id
+      genres
+    }
+  }
+`
+
 const Books = (props) => {
   const [selectedGenre, setSelectedGenre] = useState(null)
   const [allGenres, setAllGenres] = useState([])
@@ -33,12 +49,23 @@ const Books = (props) => {
     loading: booksLoading,
     error: booksError,
     data: booksData,
+    refetch,
   } = useQuery(ALL_BOOKS, {
     variables: { genre: selectedGenre },
     pollInterval: 2000,
   })
 
   const { loading: genresLoading, data: genresData } = useQuery(ALL_GENRES)
+
+  useSubscription(BOOK_ADDED, {
+    onData: ({ data }) => {
+      const newBook = data.data.bookAdded
+      if (newBook) {
+        alert(`New book added: ${newBook.title} by ${newBook.author.name}`)
+        refetch() // Refetch kirjalista kun uus kirja lisatty
+      }
+    },
+  })
 
   useEffect(() => {
     if (genresData) {
@@ -89,7 +116,7 @@ const Books = (props) => {
             <th>published</th>
           </tr>
           {booksData.allBooks.map((book) => (
-            <tr key={book.title}>
+            <tr key={book.id}>
               <td>{book.title}</td>
               <td>{book.author.name}</td>
               <td>{book.published}</td>
